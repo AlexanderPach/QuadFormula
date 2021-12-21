8598ee923647a94656093b5bcc6d7d6d001c5243
AlexanderPach <33683097+AlexanderPach@users.noreply.github.com>
2021-12-20 23:41:39 -0700

    Update QuadForm.java
    Saving for future project

--- QuadForm.java
+++ QuadForm.java
@@ -1,1 +1,27 @@
+public class QuadForm {
+
+  public static void main(String[] args){
+  
+    double a;
+    double b;
+    double c;
+    
+    a = Double.parseDouble(args[0]);
+    b = Double.parseDouble(args[1]);
+    c = Double.parseDouble(args[2]);
+    
+    double discriminant = (b**2.0) - 4.0*(a*c);
+    double numerator = Math.sqrt(discriminant);
+    
+    double root_a = (-b + numerator)/2.0 * a;
+    double root_b = (-b - numerator)/2.0 * a;
+    
+    System.out.println(root_a);
+    System.out.println(root_b);
+    
+  
+  }
+
+}
+
 
